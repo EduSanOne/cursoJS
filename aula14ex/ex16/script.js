4d82260c32bd0237aa11adbfe18841dd90d1582e
EduSanOne <122896144+EduSanOne@users.noreply.github.com>
2025-12-21 21:53:27 -0300

    1

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -5,16 +5,16 @@ function contar(){
 
     var res = window.document.getElementById('res')
 
-    if (ini === '' || fim === '' || pas === '') {
+    if (ini === '' || fim === '' || pas == '') {
         window.alert('[ERRO] - Insira números nos campos abaixo! ')
     }else if (ini <= fim) {
         for (ini;ini <= fim; ini += pas) {
         res.innerHTML += `${ini} 👉`
-    }
+        }
     } else {
         for (ini;ini >= fim; ini -= pas) {
         res.innerHTML += `${ini} 👉` 
-    }
+        }
     }
     res.innerHTML += `🎌`
 }
