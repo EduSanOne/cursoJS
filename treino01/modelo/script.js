b34f4d6d98b27e4c83f94a5ae2d024eb59fdd18e
EduSanOne <122896144+EduSanOne@users.noreply.github.com>
2025-12-30 09:56:30 -0300

    3

--- treino01/modelo/script.js
+++ treino01/modelo/script.js
@@ -20,16 +20,37 @@ function inLista(n, valores){
 }
 
 function adicionar(){
+    res.innerHTML = ''
     if (isNumero(num.value) && !inLista(num.value, valores)){
-        valores.push(num.value)
+        valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Número ${num.value} adicionado.`
         lista.appendChild(item)
     } else {
         alert('Insira um número válido')
     }
+    num.focus()
+    num.value = ''
 }
 
 function finalizar(){
-    
+    let tot = valores.length
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    for (let pos in valores) {
+        soma += valores[pos]
+        if (valores[pos] > maior){
+            maior = valores[pos]
+        }
+        if (valores[pos] < menor){
+            menor = valores[pos]
+        }
+    }
+    let media = soma / tot
+    res.innerHTML += `<p>Foram ${tot} números cadastrados.</p>`
+    res.innerHTML += `<p>O maior número cadastrado é ${maior}.</p>`
+    res.innerHTML += `<p>O menor número cadastrado é ${menor}.</p>`
+    res.innerHTML += `<p>A soma de todos números cadastrados é ${soma}.</p>`
+    res.innerHTML += `<p>A média de todos números cadastrados é ${media}.</p>`
 }
