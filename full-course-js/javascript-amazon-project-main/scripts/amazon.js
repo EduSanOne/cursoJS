d6133daa87f68239fb337c72bf78544e2423d853
EduSanOne <122896144+EduSanOne@users.noreply.github.com>
2026-02-15 09:38:36 -0300

    Doing exercises

--- full-course-js/javascript-amazon-project-main/scripts/amazon.js
+++ full-course-js/javascript-amazon-project-main/scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -57,6 +57,10 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
     
+    let quantityChoosed = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
+    console.log(quantityChoosed)
+
     let matchingItem;
     
     cart.forEach((item) => {
@@ -65,12 +69,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       }
     });
 
-    if (matchingItem){
+
+    if (matchingItem && quantityChoosed == 1){
       matchingItem.quantity += 1;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: quantityChoosed
       });
     };
 
